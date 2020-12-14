112768625145215d2218adae4f8e44b2acb87402
sunhongchao <sunhongchao11@163.com>
2020-12-14 10:05:26 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>黑马头条</h1>
+    <!-- 路由出口 -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="less"></style>
